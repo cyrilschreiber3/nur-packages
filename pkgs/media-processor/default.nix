7c53496e616b5ed45b558c49e7873a523b704544
cyrilschreiber3 <contact@cyrilschreiber.ch>
2025-05-20 23:14:25 +0200

    Add media-processor package to default.nix

---
+++ pkgs/media-processor/default.nix
@@ -0,0 +1,20 @@
+# Import a flake from the media-processor directory using flake-compat
+{pkgs}: let
+  mediaProcessorSrc = pkgs.fetchFromGitHub {
+    owner = "cyrilschreiber3";
+    repo = "media-processor";
+    tag = "v0.1.0";
+    hash = "sha256-TVCdir41A4zk8cR/IejfDKGhuGUCX4w1OlnWdL/Bl28=";
+  };
+
+  mediaProcessor =
+    (import (pkgs.fetchFromGitHub {
+        owner = "edolstra";
+        repo = "flake-compat";
+        rev = "9100a0f413b0c601e0533d1d94ffd501ce2e7885";
+        hash = "sha256-CIVLLkVgvHYbgI2UpXvIIBJ12HWgX+fjA8Xf8PUmqCY=";
+      }) {
+        src = mediaProcessorSrc;
+      }).defaultNix;
+in
+  mediaProcessor.packages.${pkgs.system}.default
